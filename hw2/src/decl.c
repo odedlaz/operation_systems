72c5ad342e18aca398e127aff652c87c7a187b3f
Oded Lazar <odedlaz@gmail.com>
2017-05-03 13:22:55 +0300

    [hw2] add questions :gift:

---
+++ hw2/src/decl.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define TMin INT_MIN
+#define TMax INT_MAX
+
+#include "btest.h"
+#include "bits.h"
+
+test_rec test_set[] = {
+
+
+
+
+
+ {"float_neg", (funct_t) float_neg, (funct_t) test_float_neg, 1,
+    "$", 10, 2,
+     {{1, 1},{1,1},{1,1}}},
+ {"float_i2f", (funct_t) float_i2f, (funct_t) test_float_i2f, 1,
+    "$", 30, 4,
+     {{1, 1},{1,1},{1,1}}},
+ {"float_twice", (funct_t) float_twice, (funct_t) test_float_twice, 1,
+    "$", 30, 4,
+     {{1, 1},{1,1},{1,1}}},
+ {"float_half", (funct_t) float_half, (funct_t) test_float_half, 1,
+    "$", 30, 4,
+     {{1, 1},{1,1},{1,1}}},
+ {"float_f2i", (funct_t) float_f2i, (funct_t) test_float_f2i, 1,
+    "$", 30, 4,
+     {{1, 1},{1,1},{1,1}}},
+ {"float_abs", (funct_t) float_abs, (funct_t) test_float_abs, 1,
+    "$", 10, 2,
+     {{1, 1},{1,1},{1,1}}},
+  {"", NULL, NULL, 0, "", 0, 0,
+   {{0, 0},{0,0},{0,0}}}
+};
