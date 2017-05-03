928865696be2805d10c592144ac07c6dc8822f40
Oded Lazar <odedlaz@gmail.com>
2017-05-03 13:22:55 +0300

    [hw2] add questions :gift:

---
+++ hw2/src/bits.h
@@ -0,0 +1,13 @@
+
+unsigned float_neg(unsigned);
+unsigned test_float_neg(unsigned);
+unsigned float_i2f(int);
+unsigned test_float_i2f(int);
+unsigned float_twice(unsigned);
+unsigned test_float_twice(unsigned);
+unsigned float_half(unsigned);
+unsigned test_float_half(unsigned);
+int float_f2i(unsigned);
+int test_float_f2i(unsigned);
+unsigned float_abs(unsigned);
+unsigned test_float_abs(unsigned);
---
+++ hw2/src/tests.c
@@ -0,0 +1,72 @@
+/* Testing Code */
+
+#include <limits.h>
+#include <math.h>
+
+/* Routines used by floation point test code */
+
+/* Convert from bit level representation to floating point number */
+float u2f(unsigned u) {
+  union {
+    unsigned u;
+    float f;
+  } a;
+  a.u = u;
+  return a.f;
+}
+
+/* Convert from floating point number to bit-level representation */
+unsigned f2u(float f) {
+  union {
+    unsigned u;
+    float f;
+  } a;
+  a.f = f;
+  return a.u;
+}
+
+unsigned test_float_neg(unsigned uf) {
+    float f = u2f(uf);
+    float nf = -f;
+    if (isnan(f))
+ return uf;
+    else
+ return f2u(nf);
+}
+unsigned test_float_i2f(int x) {
+  float f = (float) x;
+  return f2u(f);
+}
+unsigned test_float_twice(unsigned uf) {
+  float f = u2f(uf);
+  float tf = 2*f;
+  if (isnan(f))
+    return uf;
+  else
+    return f2u(tf);
+}
+unsigned test_float_half(unsigned uf) {
+  float f = u2f(uf);
+  float hf = 0.5*f;
+  if (isnan(f))
+    return uf;
+  else
+    return f2u(hf);
+}
+int test_float_f2i(unsigned uf) {
+  float f = u2f(uf);
+  int x = (int) f;
+  return x;
+}
+
+unsigned test_float_abs(unsigned uf) {
+  float f = u2f(uf);
+  unsigned unf = f2u(-f);
+  if (isnan(f))
+    return uf;
+  /* An unfortunate hack to get around a limitation of the BDD Checker */
+  if ((int) uf < 0)
+      return unf;
+  else
+      return uf;
+}
