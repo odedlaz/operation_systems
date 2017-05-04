2d6ba8ce4c1fdac9f2f303d7c1489a6b69bc1625
Oded Lazar <odedlaz@gmail.com>
2017-05-04 18:30:16 +0300

    [hw2] add ids :pray:

--- hw2/src/bits.c
+++ hw2/src/bits.c
@@ -1,7 +1,8 @@
 /*
  * CS:APP Data Lab
  *
- * <Please put your name and userid here>
+ * Afik Bar 311121289
+ * Oded Lazar 201285509
  *
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
